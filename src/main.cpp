5461581dd53bc0d3711eac3d8b8228e1e27aee6b
Myles Pasetsky <mp3blue99@gmail.com>
2021-09-12 13:05:12 -0400

    Add opcode parser helper

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,33 @@
 #include <iostream>
 
 #include "./hardware/cpu.hpp"
+#include "./opcode.hpp"
 
 int main()
 {
     chippy::cpu cpu;
 
-    if (!cpu.load_rom("./roms/tetris.rom"))
+    if (!cpu.mem.load_rom("./roms/tetris.rom"))
     {
         std::cout << "Couldn't load rom :( \n";
         return 1;
     }
 
-    // NOTE: the instructions are big-endian, so 0xe000 -> 0x00e0
     // cpu.load_rom(std::vector<std::uint16_t>({ 
     //     0xee00, 0xe000, 0x55f2
     // }));
 
     for (int i = 0; i < 3; ++i) 
     {
-        auto opcode = cpu.get_opcode();
+        chippy::opcode opcode{cpu.get_opcode()};
 
         std::cout << "READ: ";
         std::cout << std::hex;
 
-        std::cout << ((opcode&0xF000)>>12);
-        std::cout << ((opcode&0x0F00)>> 8);
-        std::cout << ((opcode&0x00F0)>> 4);
-        std::cout << ((opcode&0x000F)>> 0);
+        std::cout << (opcode.extract(3));
+        std::cout << (opcode.extract(2));
+        std::cout << (opcode.extract(1));
+        std::cout << (opcode.extract(0));
 
         std::cout << std::dec;
         std::cout << "\n";
---
+++ src/opcode.hpp
@@ -0,0 +1,90 @@
+#ifndef __OPCODE_HPP__
+#define __OPCODE_HPP__
+
+#include <cstdint>
+
+namespace chippy
+{
+
+struct opcode
+{
+    /**
+     * http://devernay.free.fr/hacks/chip8/C8TECH10.HTM#3.1
+     * 
+     * opcodes are 2 bytes (4 nibbles)
+     * 
+     * the highest 4 bits is always a constant 0x0-0xF
+     * the remaining 12 bits can be a combination of:
+     *  - "nnn" -> A 12-bit value, the lowest 12 bits of the instruction
+     *  - "n" -> A 4-bit value, the lowest 4 bits of the instruction
+     *  - "x" -> A 4-bit value, the lower 4 bits of the high byte of the instruction
+     *  - "y" -> A 4-bit value, the upper 4 bits of the low byte of the instruction
+     *  - "kk" -> An 8-bit value, the lowest 8 bits of the instruction
+     *  - some constant value specific to that opcode
+     */
+
+    const std::uint16_t _opcode;
+
+    constexpr inline opcode(std::uint16_t _opcode) :
+        _opcode{_opcode}
+    {
+    }
+
+    constexpr inline std::uint16_t get() const
+    { return _opcode; }
+
+    /****************
+     * PARSE OPCODE *
+     ****************/
+
+    // highest 4 bits
+    constexpr inline std::uint8_t id() const
+    { return extract(3); }
+
+    // nnn
+    constexpr inline std::uint16_t addr() const
+    { return extract(0, 2); }
+
+    // kk 
+    constexpr inline std::uint8_t byte() const
+    { return extract(0, 1); }
+
+    // n
+    constexpr inline std::uint8_t nibble() const
+    { return extract(0); }
+
+    // x
+    constexpr inline std::uint8_t x() const
+    { return extract(2); }
+
+    // y 
+    constexpr inline std::uint8_t y() const
+    { return extract(1); }
+
+    /******************* 
+     * EXTRACT NIBBLES *
+     *******************/
+
+    constexpr inline std::uint16_t extract(std::uint8_t ln, std::uint8_t hn) const
+    {
+        std::uint16_t norm = 4 * ln;
+
+        // fix the F's in the mask
+        std::uint16_t mask = (1<<(4 * (hn-ln+1))) - 1;
+        // fix the 0's in the mask
+        mask<<=norm;
+        
+        return (_opcode&mask)>>norm;
+    }
+
+    constexpr inline std::uint16_t extract(std::uint8_t nibble) const
+    {
+        return extract(nibble, nibble);
+    }
+
+};
+
+
+}
+
+#endif
