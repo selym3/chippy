d8060f41977990520b3a6a10c53e17e1d8f2745e
Myles Pasetsky <mp3blue99@gmail.com>
2021-09-11 23:51:59 -0400

    Add cpu with minimal functionality

---
+++ src/hardware/cpu.hpp
@@ -0,0 +1,108 @@
+#ifndef __CPU_HPP__
+#define __CPU_HPP__
+
+#include <array>
+#include <string>
+#include <vector>
+
+#include <iostream>
+#include <fstream>
+
+namespace chippy
+{
+    // Registers from V0-VF
+    constexpr std::size_t kRegisters = 0x10;
+    using registers_t = std::array<std::uint8_t, kRegisters>;
+
+    // 16 16-bit value stack
+    constexpr std::size_t kStackSize = 16;
+    using stack_t = std::array<std::uint16_t, kStackSize>;
+
+    // memory for 0x000-0xFFF (inclusive)
+    constexpr std::size_t kBufferBytes = 0x1000;
+    using memory_t = std::array<char, kBufferBytes>; //std::vector<char>;
+
+    struct cpu
+    {
+        // MEMORY
+        memory_t mem;
+
+        // REGISTERS
+        registers_t v; // variables (0x0-0xF)
+        std::uint16_t address; // address
+        std::uint8_t pc; // program counter
+
+        // STACK
+        std::uint8_t sp; // stack pointer
+        stack_t stack; // stack
+
+        // TIMERS
+        std::uint8_t delay; // delay timer (60 hz)
+        std::uint8_t sound; // sound timer (60hz)
+
+        // ROM
+        bool load_rom(const std::string &rompath)
+        {
+            std::ifstream file;
+            file.open(rompath, std::ios::in);
+
+            if (!file.is_open())
+            {
+                std::cerr << "Failed to open " << rompath << "\n";
+                return false;
+            }
+
+            for (std::size_t i = 0x200;; ++i)
+            {
+                // get byte pairs & check for error
+                char nextbyte[1];
+                file.read(nextbyte, 1);
+
+                // maybe do a size check to see if it was
+                // a bad error instead of runnign out of stuff
+                if (!file.good())
+                    break;
+
+                // store byte in memory
+                mem[i] = nextbyte[0];
+            }
+
+            file.close();
+            return true;
+        }
+
+        bool load_rom(const std::vector<char> &rombytes)
+        {
+            for (std::size_t i = 0; i < rombytes.size(); ++i)
+                mem[i + 0x200] = rombytes[i];
+
+            return true;
+        }
+
+        bool load_rom(const std::vector<std::uint16_t> &rombytes)
+        {
+            for (std::size_t i = 0; i < rombytes.size(); ++i)
+            {
+                auto opcode = rombytes[i];
+
+                mem[(i * 2) + 0x200] = (opcode & 0xFF00) >> 8;
+                mem[(i * 2) + 0x201] = (opcode & 0x00FF) >> 0;
+            }
+
+            return true;
+        }
+    
+        // OPCODES
+        std::uint16_t get_opcode() const
+        {
+            char upper = mem[0x200 + pc+0];
+            char lower = mem[0x200 + pc+1];
+
+            return (static_cast<unsigned char>(upper)<<8) +
+                   (static_cast<unsigned char>(lower)<<0);
+        }
+
+    };
+}
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -5,75 +5,35 @@
 #include <algorithm>
 #include <string>
 
-char itohc(std::uint8_t a) 
-{
-    if (a < 10)
-        return a+'0';
-    else
-        return (a-10)+'A';
-} 
 
-std::string hex(std::uintmax_t a) 
-{
-    std::string hex;
-
-    do {
-        hex += itohc(a%16);
-        a/=16;
-    } while (a>=1);
-
-    std::reverse(hex.begin(), hex.end());
-    return hex;
-}
-
+#include "./hardware/cpu.hpp"
 
 int main()
 {
-    // Create file stream
-    auto filename = "./roms/tetris.rom";
-    std::ifstream file(
-        filename,
-        std::ios::binary | std::ios::in
-    );
-
-    if (!file.is_open())
+    chippy::cpu cpu;
+    if (!cpu.load_rom("./roms/tetris.rom"))
     {
-        std::cerr << "Failed to open " << filename;
+        std::cout << "Couldn't load rom :( \n";
         return 1;
     }
 
-    // Read file stream
-    using Chunk = std::uint16_t;
-    using Byte = std::uint8_t;
-
-    for (;;)
+    for (int i = 0; i < 5; ++i) 
     {
-        // Read next chunk
-        // Chunk chunk = 0;
-        // file.read(reinterpret_cast<char*>(chunk), 2);
+        auto opcode = cpu.get_opcode();
 
-        char buffer[2];
-        file.read(buffer, 2);
-
-        Chunk chunk = (static_cast<Byte>(buffer[0]) << 8) +
-                      (static_cast<Byte>(buffer[1]) << 0);
-
-        // Print chunk as nibbles
         std::cout << "READ: ";
+        std::cout << std::hex;
 
-        std::cout << hex((chunk & 0xF000) >> 12) << " ";
-        std::cout << hex((chunk & 0x0F00) >>  8) << " ";
-        std::cout << hex((chunk & 0x00F0) >>  4) << " ";
-        std::cout << hex((chunk & 0x000F) >>  0) << " ";
+        std::cout << ((opcode&0xF000)>>12);
+        std::cout << ((opcode&0x0F00)>> 8);
+        std::cout << ((opcode&0x00F0)>> 4);
+        std::cout << ((opcode&0x000F)>> 0);
 
+        std::cout << std::dec;
         std::cout << "\n";
 
-        // Check error bits for stopping point
-        if (!file.good())
-            break;
+        cpu.pc += 2;
     }
-
-    file.close();
 
     return 0;
 }
