ad9d168946231928f1932f4cf070acef00f9ea42
Myles Pasetsky <mp3blue99@gmail.com>
2021-09-19 22:29:14 -0400

    Update main prog, compiler options

--- chippy/main.cpp
+++ chippy/main.cpp
@@ -2,47 +2,48 @@
 
 #include "./cpu.hpp"
 
+bool use_program(chippy::memory_t&, std::string);
+bool use_counter(chippy::memory_t&);
+
 int main()
 {
     chippy::cpu cpu;
     
-    // if (!cpu.memory.load_rom("./roms/tetris.rom"))
-    // {
-    //     std::cout << "Couldn't load rom :( \n";
-    //     return 1;
-    // }
-
-    // cpu.memory.load_rom(
-    //     std::vector<std::uint16_t>({
-    //         0xA000 + 0x0200 + 0x0009, // set address register to start of sprite data
-    //         0x2000 + 0x0200 + 0x0004, // call subroutine starting at 0x204
-    //         0x00e0, // clear display 
-    //         0xD004, // draw command
-    //         0x00ee, // return to draw subroutine
-
-    //         // sprite data (4 bytes)
-    //         0b0000000100000010,
-    //         0b0000001100000100,
-    //     }),
-    //     true
-    // );
-
-    cpu.memory.load_rom(
-        std::vector<std::uint16_t>({
-            0xA000 + 0x0200 + 0x0006,
-            0xD004,
-            0x1000 + 0x0200 + 0x0002,
-            0b0000000100000010,
-            0b0000001100000100,
-        }),
-        true
-    );
+    if (!use_counter(cpu.memory)) 
+        std::cerr << "failed to load program\n";
 
     for (;;)
     {
-        cpu.cycle();
         std::cin.get();
+        cpu.cycle();
     }
 
     return 0;
+}
+
+bool use_counter(chippy::memory_t& memory)
+{
+    return memory.load_rom(
+        std::vector<std::uint16_t>({
+            0x6000, // V0 - which digit
+            0x6301, // V3 - increment for digit (1)
+            0x6100, // V1 - x-cor of sprite
+            0x6200, // V2 - y-cor of sprite
+            
+            0xF029, // set addr reg to digit
+            0x00e0, // clear screen
+            0xD125, // draw digit at x, y cors
+
+            0x8034, // V0 += V3 (digit += 1)
+            0x4010, // if V0 != 16
+            0x6000, // set V0 to 0
+            0x1200 + 4*2, // jump 4 instructions from start
+        }),
+        true
+    );
+}
+
+bool use_program(chippy::memory_t& memory, std::string fname)
+{
+    return memory.load_rom(fname);
 }
